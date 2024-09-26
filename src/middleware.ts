26e53f903aea2afee980142034c5efbfbcd16f43
alnnu <luannf1020@gmail.com>
2024-09-26 13:08:42 -0300

    new middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,17 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import type {NextRequest} from 'next/server'
+import {NextResponse} from 'next/server'
 
-export default async function middleware(
-    req: NextRequest
-): Promise<NextResponse | null> {
-    // const jwt = req.cookies.get("next-auth.session-token");
-    //
-    // const { pathname } = req.nextUrl;
-    // const routesToBlock = ["/", "/transaction", "/categories", "/accounts", "/setting"]
-    // if (!pathname.endsWith("/") &&  pathname !== "/signin") {
-    //     if (!jwt) {
-    //         req.nextUrl.pathname = "/signin";
-    //         return NextResponse.redirect(req.nextUrl.href);
-    //     }
-    // }
-    //
-    // if (pathname.startsWith("/signin")) {
-    //     if (jwt) {
-    //         req.nextUrl.pathname = "/";
-    //         return NextResponse.redirect(req.nextUrl.href);
-    //     }
-    // }
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+    const jwt = request.cookies.get("next-auth.session-token");
+    if (!jwt) {
+        request.nextUrl.pathname = "/signin";
+        return NextResponse.redirect(request.nextUrl.href);
+    }
+    return null
+}
 
-    return null; // Return null to indicate no redirection
+// See "Matching Paths" below to learn more
+export const config = {
+    matcher: ["/", "/transaction", "/categories", "/accounts", "/setting"],
 }
